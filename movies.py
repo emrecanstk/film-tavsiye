3c3604e8d6ae4eeaa553957a540f28b3c0d02361
emrecanstk <mkpo.satemr06@gmail.com>
2021-02-16 21:25:26 +0300

    film sayısına değişken atadık

--- movies.py
+++ movies.py
@@ -1,15 +1,20 @@
 bilgilendirme = """
 Geliştirici: Emre Can Satık
 Adres: https://www.github.com/emrecanstk
-Program Sürümü 1.1 """
+Program Sürümü 1.2 """
 print(bilgilendirme+"\n")
 from math import *
+from os import close
 user_list,film_list,begeni_list,aksiyon_list,komedi_list,korku_list = [],[],[],[],[],[]
 tavsiye_list,tfilm_list,tbegeni_list,taksiyon_list,tkomedi_list,tkorku_list = [],[],[],[],[],[]
 dict1,dict2,farklar = {},{},list()
 
-def veriler():
-    with open("veri_filmler.txt","r",encoding="utf-8") as filmler:
+filmler = open("C:\\Repo\\film-tavsiye\\veri_filmler.txt","r",encoding="utf-8")
+fs = filmler.read().count("\n")  # fs = film sayısı
+filmler.close()
+
+def veriler():     # KENDİ DOSYA YOLUNUZU YAZINIZ.       # film listemizi parçalar halinde alıyoruz
+    with open("C:\\Repo\\film-tavsiye\\veri_filmler.txt","r",encoding="utf-8") as filmler:
         for satir in filmler:
             satir = satir[:-1]
             liste = satir.split(":")        # her bir satır için filmlerle puanları birbirinden ayırdık.
@@ -22,9 +27,9 @@ def veriler():
             komedi_list.append(komedi)
             korku = int(puanlar[2])
             korku_list.append(korku)
-        
-def tveriler():                             # yukarıdaki işlemleri burada da uyguladık. 
-    with open("veri_tavsiye.txt","r",encoding="utf-8") as tavsiyeler:
+
+def tveriler():     # KENDİ DOSYA YOLUNUZU YAZINIZ.      # yukarıdaki işlemleri tavsiye filmler için de uyguluyoruz 
+    with open("C:\\Repo\\film-tavsiye\\veri_tavsiye.txt","r",encoding="utf-8") as tavsiyeler:
         for satir in tavsiyeler:
             satir = satir[:-1]
             liste = satir.split(":")
@@ -49,18 +54,18 @@ def tercih_belirt():
 def tercih_kaydet(): # ileriki aşamada programın kendisini düzenleyebilmesi için verilerin tutulması:
     user_isim = input("isminiz: ")
     user_list.append(user_isim)
-    with open("veri_tercihler.txt","a",encoding="utf-8") as tercihler:
-        a = 0
-        while a<12:
+    with open("C:\\Repo\\film-tavsiye\\veri_tercihler.txt","a",encoding="utf-8") as tercihler:
+        a = 0          # KENDİ DOSYA YOLUNUZU YAZINIZ.
+        while a<fs:
             tercihler.write(f"{user_isim}:{film_list[a]}:{begeni_list[a]}\n")
             a += 1
 
 def tavsiye_yap():
-    a,b,c,d = 0,0,0,0
+    a,b,d = 0,0,0
     aksiyon_score,komedi_score,korku_score,tavsiye_score,tfilmler_dict = [],[],[],[],{}
     toplam_aksiyon,toplam_komedi,toplam_korku,toplam_aksiyon_score,toplam_komedi_score,toplam_korku_score = 0,0,0,0,0,0
 
-    while a<12:
+    while a<fs:
         toplam_aksiyon += aksiyon_list[a]
         toplam_komedi += komedi_list[a]
         toplam_korku += korku_list[a]
@@ -76,18 +81,15 @@ def tavsiye_yap():
     komedi_istek = toplam_komedi_score / toplam_komedi
     korku_istek = toplam_korku_score / toplam_komedi
 
-    while b<12:
+    while b<fs:
         tavsiye_score.append((aksiyon_istek*taksiyon_list[b])+(komedi_istek*tkomedi_list[b])+(korku_istek*tkorku_list[b]))
+        tfilmler_dict[tavsiye_score[b]] = tfilm_list[b]
         b += 1
-    
-    while c<12:
-        tfilmler_dict[tavsiye_score[c]] = tfilm_list[c]
-        c += 1
-    
+
     tavsiye_score.sort()
     tavsiye_score.reverse()
 
-    while d<12:
+    while d<fs:
         tavsiye_list.append(tfilmler_dict[tavsiye_score[d]])
         print(f"{d+1}- {tavsiye_list[d]}")
         d += 1
@@ -100,19 +102,19 @@ def tavsiye_degerlendir():
 def raporlar():
     tespit_basari = 0.0
     a,b,c,d=0,0,0,0
-    
-    while a<12:
+
+    while a<fs:
         dict1[tbegeni_list[a]] = a+1
         a += 1
     
     tbegeni_list.sort()
     tbegeni_list.reverse()
 
-    while b<12:
+    while b<fs:
         dict2[tbegeni_list[b]] = b+1
         b += 1
 
-    while c<12:
+    while c<fs:
         fark = dict1[tbegeni_list[c]] - dict2[tbegeni_list[c]]
         farklar.append(abs(fark))
         c += 1
@@ -122,31 +124,22 @@ def raporlar():
         toplam_fark += farklar[d]
         d += 1
 
-    tespit_basari = 100 - ((100*toplam_fark)/72)
-    '''
-    # Film sayısının değişken olması durumunda aşağıdaki algoritmadan yararlanılabilir.
-    a => eleman sayısı.
-    toplam*2 => o elemanlarla oluşabilecek maksimum fark toplamı.
-    Not: Bu algoritmada da görüleceği üzere 12 elemanlı bir listede oluşabilecek maks fark 72'dir.
-
     a,toplam = 2,0
-    while a<=12:
+    while a<=fs:
         bir_eksik = a-1
         while bir_eksik > 0:
             toplam += bir_eksik
             bir_eksik -= 2
-        print(toplam*2)
+        max_fark = toplam*2
         toplam = 0
         a += 1
-    '''
-    # başarı oranını hali hazırdaki çalıştırma için, bir de şimdiye kadarki tüm çalıştırmalar için ayrı ayrı topla.
-    # belli periyotlar arasında başarı oranının değişimini de ekleyebiliriz.(geliştirici için)
+
+    tespit_basari = 100 - ((100*toplam_fark)/max_fark)
 
     tespit_basari = round(tespit_basari,2)
-    with open("veri_raporlar.txt","a") as raporlar:  # raporların kaydedilmesi
-        raporlar.write(str(tespit_basari)+" ")
+    with open("C:\\Repo\\film-tavsiye\\veri_raporlar.txt","a") as raporlar:  # raporların kaydedilmesi
+        raporlar.write(str(tespit_basari)+" ")  
     
-    # sonuc:
     print("Bu çalıştırma için başarı oranı: %"+str(tespit_basari)+"\n")
     
 print("\n--Aşağıdaki filmleri tam sayılar kullanarak puanlayınız--\n")
